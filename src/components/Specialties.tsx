daf23a74baed99bbd1d03d0a8a93102088a06e92
luigiikk <luigimee15@gmail.com>
2024-08-11 11:46:25 -0300

    fix: finish style specialties section

--- src/components/Specialties.tsx
+++ src/components/Specialties.tsx
@@ -1,3 +1,7 @@
+import { CgWebsite } from "react-icons/cg";
+import { CiShop } from "react-icons/ci";
+import { MdOutlineMonitor } from "react-icons/md";
+
 export const Specialties = () => {
     return (
       <>
@@ -7,16 +11,21 @@ export const Specialties = () => {
           </h1>
   
           <div className="flex flex-col items-center xl:flex-row gap-12 xl:gap-56 ">
-            <div className="flex flex-col justify-center gap-1 border  rounded-lg w-full h-64 p-6 transition-transform transform hover:scale-105 hover:bg-opacity-75 hover:border-white hover:shadow-lg hover:shadow-white">
-              <h2 className="font-bold">Website</h2>
+            <div className="flex flex-col justify-center gap-2 rounded-lg w-full h-64 p-6 transition-transform transform hover:scale-105 hover:bg-opacity-75 hover:border-white hover:shadow-sm hover:shadow-white">
+            <CgWebsite className="text-blue-500 w-20 h-20 " />
+              <h2 className="font-bold text-2xl">Website</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
-            <div className="flex flex-col justify-center gap-1 border rounded-lg w-full h-64 p-6 transition-transform transform hover:scale-105 hover:bg-opacity-75 hover:border-white hover:shadow-lg hover:shadow-white">
-              <h2 className="font-bold">Loja Online</h2>
+
+            <div className="flex flex-col justify-center gap-2 rounded-lg w-full h-64 p-6 transition-transform transform hover:scale-105 hover:bg-opacity-75 hover:border-white hover:shadow-sm hover:shadow-white">
+              <CiShop className="text-blue-500 w-20 h-20 "/>
+              <h2 className="font-bold text-2xl">Loja Online</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
-            <div className="flex flex-col justify-center gap-1 border rounded-lg w-full h-64 p-6 transition-transform transform hover:scale-105 hover:bg-opacity-75 hover:border-white hover:shadow-lg hover:shadow-white">
-              <h2 className="font-bold">Landing Page</h2>
+
+            <div className="flex flex-col justify-center gap-2 rounded-lg w-full h-64 p-6 transition-transform transform hover:scale-105 hover:bg-opacity-75 hover:border-white hover:shadow-sm hover:shadow-white">
+              <MdOutlineMonitor className="text-blue-500 w-20 h-20 " />
+              <h2 className="font-bold text-2xl">Landing Page</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
           </div>
